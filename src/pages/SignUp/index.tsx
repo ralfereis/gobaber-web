6f838216493b65a43ea2be92103ebe82b2f7ba94
Ralfe Reis <ralfe.cavalcante@gmail.com>
2020-05-15 17:49:09 -0300

    Validating Sign Up Lesson Finished

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <img src="" alt="" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiMail} placeholder="Nome" />
+          <Input name="email" icon={FiUser} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="logon">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+export default SignUp;
