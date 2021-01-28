4672603caa2821777c4e653710dbc251f5825206
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-01-28 01:08:44 -0300

    Implematation Page

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -7,7 +7,6 @@ import {
 } from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
-
 import { Container } from './styles';
 
 interface ToastProps {
@@ -17,8 +16,8 @@ interface ToastProps {
 
 const icons = {
   info: <FiInfo size={24} />,
-  error: <FiAlertCircle size={24} />,
   success: <FiCheckCircle size={24} />,
+  error: <FiAlertCircle size={24} />,
 };
 
 const Toast: React.FC<ToastProps> = ({ message, style }) => {
@@ -28,6 +27,7 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
     const timer = setTimeout(() => {
       removeToast(message.id);
     }, 3000);
+
     return () => {
       clearTimeout(timer);
     };
@@ -40,16 +40,16 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
       style={style}
     >
       {icons[message.type || 'info']}
-
       <div>
-        <strong>{message.title} </strong>
+        <strong>{message.title}</strong>
         {message.description && <p>{message.description}</p>}
       </div>
 
-      <button onClick={() => removeToast(message.id)} type="button">
+      <button type="button" onClick={() => removeToast(message.id)}>
         <FiXCircle size={18} />
       </button>
     </Container>
   );
 };
+
 export default Toast;
